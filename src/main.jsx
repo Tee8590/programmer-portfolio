fff60bd18c6267e5474dec457e74675140a9aae1
tomgodsalo <tomgodsalo1@gmail.com>
2025-02-05 16:20:02 +0530

    ff

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <HashRouter basename={import.meta.env.BASE_URL}>
+  <HashRouter basename="/programmer-portfolio">
     <Routes>
       <Route path="*" element={<App />}>
       </Route>

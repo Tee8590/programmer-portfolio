d780717fadcae3bf710d694f0915251c9da51d20
tomgodsalo <tomgodsalo1@gmail.com>
2025-02-05 17:23:03 +0530

    df

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <HashRouter basename="{import.meta.env.BASE_URL}">
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <Routes>
       <Route path="*" element={<App />}>
       </Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
   // </React.StrictMode>
 );
 
